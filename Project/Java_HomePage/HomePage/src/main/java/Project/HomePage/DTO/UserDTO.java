9b025f4f0978efaa79a9bc9f1e86bf5a59342642
Funkyplayer95 <funkyplayer95@kakao.com>
2024-02-16 01:51:04 +0900

    java 수정
    기본 주석 작성.
    불필요 코드 제거.

--- Project/Java_HomePage/HomePage/src/main/java/Project/HomePage/DTO/UserDTO.java
+++ Project/Java_HomePage/HomePage/src/main/java/Project/HomePage/DTO/UserDTO.java
@@ -8,13 +8,12 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 public class UserDTO {
-
-    private String userId;
-    private String userPassword;
-    private String userName;
-    private String userEmail;
-    private String userPhone;
-    private int userImageId;
-    private String profileImage;
-    private String imageExtension;
+    //내가 필요한 정보만 진행.
+    private String userId; // 유저 아이디(findById 사용을 위해)
+    private String userPassword; // 유저 비밀번호(변경시 필요)
+    private String userName; // 유저 이름 (마이페이지)
+    private String userEmail; // 유저 이메일 (마이페이지)
+    private String userPhone; // 유저 전화번호(변경시 필요)
+    private String profileImage; // 유저 대표이미지 (변경시 필요)
+    private String imageExtension; // 유저 확장자 (변경시 필요)
 }
--- Project/Java_HomePage/HomePage/src/main/java/Project/HomePage/Entity/User.java
+++ Project/Java_HomePage/HomePage/src/main/java/Project/HomePage/Entity/User.java
@@ -5,28 +5,28 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "userinfo")
-@ToString
+@Entity // entity 설정
+@Getter // lombok getter 설정
+@Setter // lombok setter 설정
+@AllArgsConstructor // 모든 인자를 가진 객체 생성
+@NoArgsConstructor // 아무 인자도 없는 객체 생성
+@Table(name = "userinfo") // DB테이블명 연동
+@ToString // ToString 생성
 public class User {
 
-    @Id
-    private String user_id;
-    private String user_password;
-    private String user_name;
-    private char user_gender;
-    private int user_rrn1;
-    private int user_rrn2;
-    private String user_email;
-    private int user_address_num;
-    private String user_address_doro;
-    private String user_address_jibun;
-    private String user_address_detail;
-    private String user_phone;
-    private String profile_image;
-    private String imageExtension;
+    @Id // 기본키 지정 (이 키를 이용하여 테이블 찾음)
+    private String user_id; //사용자 아이디
+    private String user_password; //사용자 비밀번호
+    private String user_name; // 사용자 이름
+    private char user_gender; // 사용자 성별
+    private int user_rrn1; // 주민등록번호 앞자리
+    private int user_rrn2; // 주민등록번호 뒷자리
+    private String user_email; // 사용자 이메일
+    private int user_address_num; // 사용자 주소 번호
+    private String user_address_doro; // 사용자 도로명 주소
+    private String user_address_jibun; // 사용자 지번 주소
+    private String user_address_detail; // 사용자 상세 주소
+    private String user_phone; // 사용자 전화번호
+    private String profile_image; // 사용자 대표 이미지
+    private String imageExtension; // 사용자 대표 이미지 확장자
 }

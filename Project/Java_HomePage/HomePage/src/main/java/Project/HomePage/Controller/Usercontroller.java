b8d364f981b366f6b2080367680e8464bc484db3
Funkyplayer95 <funkyplayer95@kakao.com>
2024-02-22 16:51:53 +0900

    주석 진행
    부가 설명 필요한 부분 주석 진행.
    불필요한 코드 제외

--- Project/Java_HomePage/HomePage/src/main/java/Project/HomePage/Controller/Usercontroller.java
+++ Project/Java_HomePage/HomePage/src/main/java/Project/HomePage/Controller/Usercontroller.java
@@ -6,12 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -23,42 +21,38 @@ public class Usercontroller {
     @Autowired
     private final UserService userService;
 
-    @RequestMapping("/mypage")
+    @RequestMapping("/mypage") // url주소에 /mypage로 이동시
     public String myPage(Model model, HttpSession session,
                          @RequestParam(value = "idvalue", required = false) String id) {
-        if (id == null) {
-            id = session.getAttribute("idvalue").toString();
+        if (id == null) { // id 값이 null이면
+            id = session.getAttribute("idvalue").toString(); // id에 idvalue로 된 세션값을 넣는다.
         }
-        session.setAttribute("idvalue", id);
-        UserDTO userDTO = userService.getUserInfo(id);
-        model.addAttribute("user", userDTO);
+        session.setAttribute("idvalue", id); // id값을 idvalue라는 이름의 세션 값으로 설정.
+        UserDTO userDTO = userService.getUserInfo(id); // id값을 받아와서 user정보를 userDTO에 저장.
+        model.addAttribute("user", userDTO); // userDTO객체를 "user"이라는 모델 속성 으로 추가.
 
-        return "mypage";
+        return "mypage"; // mypage.html로 
     }
 
 
     @RequestMapping("/updatePw")
-    public String updatePw(Model model, HttpServletRequest request, RedirectAttributes redirectAttributes, HttpSession session,
+    public String updatePw(Model model, RedirectAttributes redirectAttributes,
                            @RequestParam("idvalue2") String idValue,
                            @RequestParam("newPw") String newPw,
                            @RequestParam("re_newPw") String reNewPw) {
 
-        String id = null;
-        if (session.getAttribute("idvalue") != null) {
-            id = session.getAttribute("idvalue").toString();
-        }
+        UserDTO userDTO = userService.getUserInfo(idValue); // RequestParam으로 받은 idValue를 이용. 사용자 조회
+        model.addAttribute("user", userDTO); // user라는 모델에 정보를 주입.
 
-        UserDTO userDTO = userService.getUserInfo(idValue);
-        model.addAttribute("user", userDTO);
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // 암호화 할때 사용
 
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-        // Perform password validation
-        if (!newPw.equals(reNewPw)) {
+        if (!newPw.equals(reNewPw)) { // 새 비밀번호와 확인부분이 일치하지 않으면.
+            // errorMessage를 FlashAttribute에 지정. redirectAttributes.addFlashAttribute는 작은 양의 정보를 전달하는데 사용한다함.
             redirectAttributes.addFlashAttribute("errorMessage", "새로운 비밀번호가 일치하지 않습니다.");
-            return "redirect:/mypage";
+            return "redirect:/mypage"; // mypage로 redirect함.
         } else {
-        userService.updatePassword(idValue, newPw);
+        userService.updatePassword(idValue, newPw); // 일치한다면  idValue값의 비밀번호를 newPw로 지정.
         redirectAttributes.addFlashAttribute("successMessage", "비밀번호가 변경되었습니다.");
         }
 
@@ -70,11 +64,11 @@ public class Usercontroller {
     public String fileUpload(@RequestParam("imageFile") MultipartFile file,
                              HttpServletRequest req, RedirectAttributes redirectAttributes){
         try{
-            String id = req.getSession().getAttribute("idvalue").toString();
-            userService.imgSave(file, id);
-            redirectAttributes.addFlashAttribute("message", "이미지 업로드가 성공하였습니다!");
-        } catch (RuntimeException e){
-            redirectAttributes.addFlashAttribute("error", e.getMessage());
+            String id = req.getSession().getAttribute("idvalue").toString(); // req를 통해 세션의 idvalue을 가져온다. 
+            userService.imgSave(file, id); // imgSave 호출, 프로필 이미지 저장.
+            redirectAttributes.addFlashAttribute("message", "이미지 업로드가 성공하였습니다!"); // 업로드 성공 메세지
+        } catch (RuntimeException e){ //업로드 과정에서 예외 발생시 예외를 잡아 처리
+            redirectAttributes.addFlashAttribute("error", e.getMessage()); // 리다이렉트된 페이지에 에러메세지가 나오도록
         }
         return "redirect:/mypage";
     }
--- Project/Java_HomePage/HomePage/src/main/java/Project/HomePage/Service/UserService.java
+++ Project/Java_HomePage/HomePage/src/main/java/Project/HomePage/Service/UserService.java
@@ -22,73 +22,74 @@ public class UserService {
 
     @Autowired
     private final UserRepository userRepository;
-    private final Path root = Paths.get("uploads");
 
     public UserDTO getUserInfo(String id) {
 
         User user = userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        UserDTO userDTO = new UserDTO();
+        // 아이디가 있으면 반환. 아이디에 해당하는 사용자가 없으면 예외를 발생시킨다.
 
-        userDTO.setUserId(user.getUser_id());
-        userDTO.setUserName(user.getUser_name());
-        userDTO.setUserEmail(user.getUser_email());
-        userDTO.setUserPhone(user.getUser_phone());
-        userDTO.setProfileImage(user.getProfile_image());
-        userDTO.setImageExtension(getExtension(user.getProfile_image()));
+        UserDTO userDTO = new UserDTO(); // 새로운 DTO객체 생성
 
-        return userDTO;
+        userDTO.setUserId(user.getUser_id()); // DTO에 user_id를 set한다
+        userDTO.setUserName(user.getUser_name()); // DTO에 user_name를 set한다
+        userDTO.setUserEmail(user.getUser_email()); // DTO에 user_email를 set한다
+        userDTO.setUserPhone(user.getUser_phone()); // DTO에 user_phone를 set한다
+        userDTO.setProfileImage(user.getProfile_image()); // DTO에 user_profile_image를 set한다
+        userDTO.setImageExtension(getExtension(user.getProfile_image())); // DTO에 user_id를 set한다
+        // getExtension 메소드는 파일의 확장자를 추출해서 반환하는 메서드이다.
+
+        return userDTO; // set된 userDTO를 반환
     }
-
+    
     @Transactional //선언적 트랜잭션. 적용된 범위에서는 트랜잭션 기능이 포함된 프록시 객체가 생성되어 자동 commit/rollback 시켜준다.
     //비즈니스 로직에서 쪼갤 수 없는 하나의 작업 단위. 데이터베이스 상태변경하며 한 번에 수행되어야 한다.
     public void updatePassword(String id, String newPw) {
 
-        User user = userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        User user = userRepository.findById(id).orElseThrow(EntityNotFoundException::new); // 아이디를 통해 사용자를 찾는다
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // BCrypt 암호화 할때 사용하는 객체
 
         //더티체킹(Dirty Checking) = JPA 핵심 기능. 전체 데이터베이스 안불러와도 됨.
-        user.setUser_password(passwordEncoder.encode(newPw));
-        userRepository.save(user);
+        user.setUser_password(passwordEncoder.encode(newPw)); // 비밀번호를 저장할때 newPw를 암호화로 인코딩한 후 저장.
+        userRepository.save(user); // save문을 통해 update 시킨다.
     }
-    @Transactional
-    public void updatePhone(String id, String newPhone) {
 
-        User user = userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        user.setUser_phone(newPhone);
-        userRepository.save(user);
-    }
+    //이미지 저장하는 메서드
     public void imgSave(MultipartFile file, String id){
         try{
-            User user = userRepository.findById(id).orElseThrow();
-            String filename = file.getOriginalFilename();
-            List<String> allowedExtensions = Arrays.asList("img", "jpg", "png");
+            User user = userRepository.findById(id).orElseThrow(); // 아이디를 통해 사용자 찾기.
+            String filename = file.getOriginalFilename(); //업로드된 파일의 이름을 가져온다.
+            List<String> allowedExtensions = Arrays.asList("img", "jpg", "png"); // 업로드 허용할 확장자를 리스트로 정의.
 
+            //파일 이름에서 확장자 부분을 추출. requireNonNull는 obj가 null이면 nullpointerror를 발생해주는 메서드.
             String extension = Objects.requireNonNull(filename).substring(filename.lastIndexOf(".") + 1);
-            if (!allowedExtensions.contains(extension)) {
-                throw new RuntimeException("img, jpg, png 파일만 업로드가능합니다.");
+            if (!allowedExtensions.contains(extension)) { // 업로드된 파일의 확장자가 allowedExtensions 값이 아니라면
+                throw new RuntimeException("img, jpg, png 파일만 업로드가능합니다."); // runtimeexception을 던진다.
             }
 
             // 이미지 데이터를 Base64로 인코딩
             String encodedImage = Base64.getEncoder().encodeToString(file.getBytes());
+            // 업로드된 이미지 파일을 byte배열로 읽고, base64로 인코딩한다. 이미지 데이터를 문자열로 저장하도록 하는 방법.
 
-            user.setProfile_image(encodedImage);
+            user.setProfile_image(encodedImage); // 파일의 인코딩된 이미지 데이터를 저장
             user.setImageExtension(extension); // 파일의 확장자를 저장
-            userRepository.save(user);
-        } catch (IOException e) {
-            throw new RuntimeException("파일을 저장할 수 없습니다. 오류 : " + e.getMessage());
+            userRepository.save(user); // save문을 사용하여 update를 진행.
+        } catch (IOException e) { // 파일을 읽기/쓰기에서 발생하는 예외를 처리하는 부분
+            throw new RuntimeException("파일을 저장할 수 없습니다. 오류 : " + e.getMessage()); // errorMessage와 함께 이유를 출력
         }
     }
 
-    //확장자 명칭을 저장하는 함수
+    //확장자를 저장하는 함수
     public String getExtension(String filename) {
-        String extension = "";
-        if (filename != null) {
-            Path path = Paths.get(filename);
-            if (path.getFileName().toString().contains(".")) {
+        String extension = ""; // 확장자를 저장할 문자열을 선언. 초기화.
+        if (filename != null) { // filename이 null이 아니면
+            Path path = Paths.get(filename); // Paths.get 메서드를 이용해 파일이름을 경로로 변환.
+            if (path.getFileName().toString().contains(".")) { // 파일 이름에 "."이 포함돼 있는지 확인. "."이 있다면 확장자가 존재한다 판단
                 extension = path.getFileName().toString().substring(path.getFileName().toString().lastIndexOf(".") + 1);
+                // path.getFileName().toString().lastIndexOf(".") + 1를 통해 마지막"."의 위치를 찾고, 그 뒤의 문자열을 substring을 통해 추출한다.
+                // 추출한것을 extension에 저장한다.
             }
         }
-        return extension;
+        return extension; //저장한 extension을 반환한다.
     }
 
 }
